61815d0121cb47a42999340bb999b5b7ca3f5258
wukai5599 <wukai5599@163.com>
2017-12-25 23:34:42 +0800

    first commit

---
+++ src/main/java/com/how2java/tmall/controller/CategoryController.java
@@ -0,0 +1,100 @@
+
+package com.how2java.tmall.controller;
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.how2java.tmall.pojo.Category;
+import com.how2java.tmall.service.CategoryService;
+import com.how2java.tmall.util.ImageUtil;
+import com.how2java.tmall.util.Page;
+import com.how2java.tmall.util.UploadedImageFile;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpSession;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+
+/*
+ * 注解@Controller声明当前类是一个控制器
+注解@RequestMapping("")表示访问的时候无需额外的地址
+注解@Autowired把CategoryServiceImpl自动装配进了CategoryService 接口
+注解@RequestMapping("admin_category_list") 映射admin_category_list路径的访问
+在list方法中，通过categoryService.list()获取所有的Category对象，然后放在"cs"中，并服务端跳转到 “admin/listCategory” 视图。
+“admin/listCategory” 会根据后续的springMVC.xml 配置文件，跳转到 WEB-INF/jsp/admin/listCategory.jsp 文件
+ * 
+ * 
+ */
+
+
+@Controller
+@RequestMapping("")
+public class CategoryController {
+    @Autowired
+    CategoryService categoryService;
+  
+    @RequestMapping("admin_category_list")
+    public String list(Model model,Page page){
+        PageHelper.offsetPage(page.getStart(),page.getCount());
+        List<Category> cs= categoryService.list();
+        int total = (int) new PageInfo<>(cs).getTotal();
+        page.setTotal(total);
+        model.addAttribute("cs", cs);
+        model.addAttribute("page", page);
+        return "admin/listCategory";
+    }
+
+    @RequestMapping("admin_category_add")
+    public String add(Category c, HttpSession session, UploadedImageFile uploadedImageFile) throws IOException {
+        categoryService.add(c);
+        File  imageFolder= new File(session.getServletContext().getRealPath("img/category"));
+        File file = new File(imageFolder,c.getId()+".jpg");
+        if(!file.getParentFile().exists())
+            file.getParentFile().mkdirs();
+        uploadedImageFile.getImage().transferTo(file);
+        BufferedImage img = ImageUtil.change2jpg(file);
+        ImageIO.write(img, "jpg", file);
+        return "redirect:/admin_category_list";
+    }
+
+    @RequestMapping("admin_category_delete")
+    public String delete(int id,HttpSession session) throws IOException {
+        categoryService.delete(id);
+
+        File  imageFolder= new File(session.getServletContext().getRealPath("img/category"));
+        File file = new File(imageFolder,id+".jpg");
+        file.delete();
+
+        return "redirect:/admin_category_list";
+    }
+
+    @RequestMapping("admin_category_edit")
+    public String edit(int id,Model model) throws IOException {
+        Category c= categoryService.get(id);
+        model.addAttribute("c", c);
+        return "admin/editCategory";
+    }
+
+    @RequestMapping("admin_category_update")
+    public String update(Category c, HttpSession session, UploadedImageFile uploadedImageFile) throws IOException {
+        categoryService.update(c);
+        MultipartFile image = uploadedImageFile.getImage();
+        if(null!=image &&!image.isEmpty()){
+            File  imageFolder= new File(session.getServletContext().getRealPath("img/category"));
+            File file = new File(imageFolder,c.getId()+".jpg");
+            image.transferTo(file);
+            BufferedImage img = ImageUtil.change2jpg(file);
+            ImageIO.write(img, "jpg", file);
+        }
+        return "redirect:/admin_category_list";
+    }
+
+}
+
